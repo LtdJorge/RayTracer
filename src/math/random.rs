7a5ca723472c4eab9ac552199977ddc5f880ab8d
Jorge López <jorgekq@hotmail.es>
2023-08-30 23:38:12 +0200

    Adds random function for f32 too

--- src/math/random.rs
+++ src/math/random.rs
@@ -6,6 +6,14 @@ pub fn random_double() -> f64 {
     rand::thread_rng().gen_range(0.0..1.0)
 }
 
+pub fn random_float() -> f32 {
+    rand::thread_rng().gen_range(0.0f32..1.0f32)
+}
+
 pub fn random_double_in_range(min: f64, max: f64) -> f64 {
+    rand::thread_rng().gen_range(min..max)
+}
+
+pub fn random_float_in_range(min: f32, max: f32) -> f32 {
     rand::thread_rng().gen_range(min..max)
 }
